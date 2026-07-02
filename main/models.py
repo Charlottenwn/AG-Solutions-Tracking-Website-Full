8d94e62ad539270a7c6abc96b4fb61e1d0854af4
Charlottenwn <145743997+Charlottenwn@users.noreply.github.com>
2026-07-02 10:34:31 +0300

    added additional models for revised funcitonal requirement

--- main/models.py
+++ main/models.py
@@ -65,6 +65,20 @@ class FactoryOrder(models.Model):
     production_end_date = models.DateField(null=True, blank=True)
     status=models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at=models.DateTimeField(auto_now_add=True)
+    
+    furniture_reminder_date = models.DateField(null=True, blank=True, editable=False)
+    is_furniture_reminder_sent = models.BooleanField(default=False)
+    
+    package_clarification_reminder_date = models.DateField(null=True, blank=True, editable=False)
+    is_package_clarification_reminder_sent = models.BooleanField(default=False)
+    
+    def save(self, *args, **kwargs):
+        from datetime import timedelta
+        if self.production_start_date:
+            self.furniture_reminder_date = self.production_start_date - timedelta(days=3)
+        if self.production_end_date:
+            self.package_clarification_reminder_date = self.production_end_date
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Factory Order for {self.order}"
