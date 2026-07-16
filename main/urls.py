46253e2abb845a98c5b667b669ac6bb8748bb4a4
Charlottenwn <145743997+Charlottenwn@users.noreply.github.com>
2026-07-16 13:01:15 +0300

    fixed 404 after docker compose down

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login_page/', views.login_page, name='login_page'),
+    path('', views.login_page, name='login_page'),
     path('main_offer_page/', views.main_offer_page, name='main_offer_page'),
     path('mark_reminder_sent/<str:kind>/<int:factory_order_id>/', views.mark_reminder_sent, name='mark_reminder_sent'),
     path('recover_password_request_page/', views.recover_password_request_page, name='recover_password_request_page'),
