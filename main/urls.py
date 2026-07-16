36bf272ef0a23e942e74fcdeeefcc725fa62d3ee
Charlottenwn <145743997+Charlottenwn@users.noreply.github.com>
2026-07-16 14:30:04 +0300

    fixed web page routing issue

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_page, name='login_page'),
+    path('', views.login_page, name='login_page_root'),
+    path('login_page/', views.login_page, name='login_page'),
     path('main_offer_page/', views.main_offer_page, name='main_offer_page'),
     path('mark_reminder_sent/<str:kind>/<int:factory_order_id>/', views.mark_reminder_sent, name='mark_reminder_sent'),
-    path('recover_password_request_page/', views.recover_password_request_page, name='recover_password_request_page'),
-    path('set_new_password_page/', views.set_new_password_page, name='set_new_password_page'),
-    path('recover_password_verify_page/', views.recover_password_verify_page, name='recover_password_verify_page'),
+    path('recover_password_request_page/', views.recover_password_request, name='recover_password_request_page'),
+    path('set_new_password_page/', views.set_new_password, name='set_new_password_page'),
+    path('recover_password_verify_page/', views.recover_password_verify, name='recover_password_verify_page'),
 ]
