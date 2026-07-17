7dd8cf35d1cb18e7b87c1262ab2ffb50d60335ba
Charlottenwn <145743997+Charlottenwn@users.noreply.github.com>
2026-07-17 11:28:55 +0300

    added minimal session controls

--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+SESSION_COOKIE_AGE = 60 * 60 * 8 # 8 hours in seconds
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
