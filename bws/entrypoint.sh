c1861bf576df19bced0b6d9929a27ceed506fa75
Charlottenwn <145743997+Charlottenwn@users.noreply.github.com>
2026-08-04 13:58:56 +0300

    Rename seven_sms_api_key.txt to seven_api_key.txt typo

--- bws/entrypoint.sh
+++ bws/entrypoint.sh
@@ -18,6 +18,6 @@ fetch bc6d4366-ddf1-4720-a04c-b49301594b87 google_sheets_key.json
 fetch dbc0f755-95db-4d52-86a7-b4930155fa03 postgres_db.txt
 fetch ebce5d26-88a5-4843-b378-b49301562c64 postgres_password.txt
 fetch f44e3d07-b608-46b4-8565-b4930156541c postgres_user.txt
-fetch 5e85bdd8-0df0-4dce-b0c9-b49b00a1ec51 seven_sms_api_key.txt
+fetch 5e85bdd8-0df0-4dce-b0c9-b49b00a1ec51 seven_api_key.txt
 
 echo "All secrets fetched successfully."
